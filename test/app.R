43646f793490f91884e3eba1b2024c111db66069
Mariem Ayadi <mayadi@smith.edu>
2018-12-09 15:58:22 -0500

    Removed comma from year slider

--- test/app.R
+++ test/app.R
@@ -1,42 +1,4 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
-
-# Define UI for application that draws a histogram
-# ui <- fluidPage(
-#    
-#    # Application title
-#    titlePanel("Luminosity over time"),
-#    
-#    # Sidebar with a slider input for number of bins 
-#    sidebarLayout(
-#       sidebarPanel(
-#          sliderInput("years",
-#                      "Choose a year:",
-#                      min = 1992,
-#                      max = 2013,
-#                      value = 1)
-#       ),
-#       
-#       # Show a plot of the generated distribution
-#       mainPanel(
-#          plotOutput("dotplot"),
-#          plotOutput("dotplot2")
-#       )
-#    )
-# )
-
-
-
-
-###
 
 ui <- fluidPage(
   
@@ -53,7 +15,8 @@ ui <- fluidPage(
                        "Choose a year:",
                        min = 1992,
                        max = 2013,
-                       value = 1)
+                       value = 1,
+                       sep = "")
     ),
     column(4)
   ),
