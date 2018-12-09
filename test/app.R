c67809bdc929bffd5ddf791c6555db571fd58fb7
Mariem Ayadi <mayadi@smith.edu>
2018-12-09 14:43:35 -0500

    Add two plots on same page

--- test/app.R
+++ test/app.R
@@ -13,13 +13,13 @@ library(shiny)
 ui <- fluidPage(
    
    # Application title
-   titlePanel("Old Faithful Geyser Data"),
+   titlePanel("Luminosity over time"),
    
    # Sidebar with a slider input for number of bins 
    sidebarLayout(
       sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
+         sliderInput("years",
+                     "Choose a year:",
                      min = 1992,
                      max = 2013,
                      value = 1)
@@ -27,7 +27,8 @@ ui <- fluidPage(
       
       # Show a plot of the generated distribution
       mainPanel(
-         plotOutput("distPlot")
+         plotOutput("dotplot"),
+         plotOutput("dotplot2")
       )
    )
 )
@@ -35,12 +36,10 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
    
-   output$distPlot <- renderPlot({
+   output$dotplot <- renderPlot({
       # generate bins based on input$bins from ui.R
       lumData <- read_csv("../data/final/tun_lum_governorate_93_13_tidy.csv")
-      lumData2013 <- lumData[lumData$year == input$bins,]
-      
-      
+      lumData2013 <- lumData[lumData$year == input$years,]
       
       # x    <- faithful[, 2] 
       # bins <- seq(min(x), max(x), length.out = input$bins + 1)
@@ -51,6 +50,22 @@ server <- function(input, output) {
       ggplot(lumData2013, aes(x = mean, y = fct_reorder(NAME_1, mean))) +
         geom_point(color = "blue") + ylab("") +
         ggtitle("Luminosity per Governovates in 2013 (mean)")
+   })
+   
+   output$dotplot2 <- renderPlot({
+     # generate bins based on input$bins from ui.R
+     lumData <- read_csv("../data/final/tun_lum_governorate_93_13_tidy.csv")
+     lumData2013 <- lumData[lumData$year == input$years,]
+     
+     # x    <- faithful[, 2] 
+     # bins <- seq(min(x), max(x), length.out = input$bins + 1)
+     # 
+     # # draw the histogram with the specified number of bins
+     # hist(x, breaks = bins, col = 'darkgray', border = 'white')
+     
+     ggplot(lumData2013, aes(x = mean, y = fct_reorder(NAME_1, mean))) +
+       geom_point(color = "blue") + ylab("") +
+       ggtitle("Luminosity per Governovates in 2013 (mean)")
    })
 }
 
